f5328ed36872b38ce5124b0bd88ac61b8cdd1b89
DARSHAN R <124617137+darshanTheDeveloper@users.noreply.github.com>
2023-08-05 20:26:25 +0530

    mydata.py

---
+++ mydata.py
@@ -0,0 +1,9 @@
+data = input("Enter the data")
+text = open("file_path","w") #replace file_path with url
+text.write(data)
+text.close()
+
+
+file_text = open("file_path",'r')
+data.read()
+print(data)
